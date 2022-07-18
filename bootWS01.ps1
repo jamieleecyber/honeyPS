1340daccad9304becdbb973a6fdb9d6384c2f1fa
Jamie.Lee <jamie.tlee@outlook.com>
2022-07-18 15:44:31 +0930

    join function added

--- bootWS01.ps1
+++ bootWS01.ps1
@@ -9,6 +9,16 @@ function set-dns {
     Remove-Item 'C:\stepfile\2.txt'
 }
 
+function join-domain {
+    $dc = "testdomain.local"
+    $password = "LazyAdminPwd123!" | ConvertTo-SecureString -asPlainText –Force
+    $user = "$dc\JamieSA"
+    $creds = New-Object System.Management.Automation.PSCredential($user,$password)
+    Start-Sleep -Seconds 120
+    Add-Computer -DomainName $dc -Credential $creds -Restart -Force -Verbose
+    Remove-Item 'C:\stepfile\3.txt'
+}
+
  if (Test-Path C:\stepfile){
      if (Test-Path C:\stepfile\1.txt){
         change-name
@@ -17,7 +27,7 @@ function set-dns {
         set-dns
      }
      if (Test-Path C:\stepfile\3.txt){
-        
+        join-domain       
      }
      if (Test-Path C:\stepfile\4.txt){
         
--- bootWS02.ps1
+++ bootWS02.ps1
@@ -8,6 +8,15 @@ function set-dns {
     netsh interface ip set dns name="Ethernet" static 192.168.56.4
     Remove-Item 'C:\stepfile\2.txt'
 }
+function join-domain {
+    $dc = "testdomain.local"
+    $password = "LazyAdminPwd123!" | ConvertTo-SecureString -asPlainText –Force
+    $user = "$dc\JamieSA"
+    $creds = New-Object System.Management.Automation.PSCredential($user,$password)
+    Start-Sleep -Seconds 120
+    Add-Computer -DomainName $dc -Credential $creds -Restart -Force -Verbose
+    Remove-Item 'C:\stepfile\3.txt'
+}
 
  if (Test-Path C:\stepfile){
      if (Test-Path C:\stepfile\1.txt){
@@ -17,7 +26,7 @@ function set-dns {
         set-dns
      }
      if (Test-Path C:\stepfile\3.txt){
-        
+        join-domain
      }
      if (Test-Path C:\stepfile\4.txt){
         
