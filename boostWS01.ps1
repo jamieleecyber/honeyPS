1e93ff0e96ca794da6640134c204e9cbea6e4027
Jamie.Lee <jamie.tlee@outlook.com>
2022-07-18 18:18:42 +0930

    bug tests

---
+++ boostWS01.ps1
@@ -0,0 +1,27 @@
+function change-name {
+    Rename-Computer -NewName DC01
+    Remove-Item 'C:\stepfile\1.txt'
+    Restart-Computer
+ }
+ 
+ if (Test-Path C:\stepfile){
+     if (Test-Path C:\stepfile\1.txt){
+         change-name
+     }
+     if (Test-Path C:\stepfile\2.txt){
+         
+     }
+     if (Test-Path C:\stepfile\3.txt){
+        
+     }
+     if (Test-Path C:\stepfile\4.txt){
+        
+     }
+ }else{
+     New-Item -Path 'C:\stepfile' -ItemType Directory
+     New-Item 'C:\stepfile\1.txt'
+     New-Item 'C:\stepfile\2.txt'
+     New-Item 'C:\stepfile\3.txt'
+     New-Item 'C:\stepfile\4.txt'
+     change-name
+ }
