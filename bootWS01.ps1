f95235707ff5572a4e5286f41670ffce5d972afb
Jamie.Lee <jamie.tlee@outlook.com>
2022-07-19 11:49:41 +0930

    bug fixes

--- bootWS01.ps1
+++ bootWS01.ps1
@@ -1,22 +1,23 @@
 function change-name {
-    Rename-Computer -NewName DC01
+    Rename-Computer -NewName WS01
     Remove-Item 'C:\stepfile\1.txt'
     Restart-Computer
  }
-
- powershell.exe -ExecutionPolicy Bypass
+ 
+ 
  if (Test-Path C:\stepfile){
      if (Test-Path C:\stepfile\1.txt){
          change-name
      }
      if (Test-Path C:\stepfile\2.txt){
-         
+         install-ad
      }
      if (Test-Path C:\stepfile\3.txt){
-        
+        create-domain
      }
      if (Test-Path C:\stepfile\4.txt){
-        
+        Remove-Item 'C:\stepfile\4.txt'
+        Restart-Computer
      }
  }else{
      New-Item -Path 'C:\stepfile' -ItemType Directory
