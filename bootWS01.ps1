0cc48da8cf14f402a93ecedec4185fe8aee0b531
Jamie.Lee <jamie.tlee@outlook.com>
2022-07-20 07:40:22 +0930

    bug fixes

--- bootWS01.ps1
+++ bootWS01.ps1
@@ -9,15 +9,6 @@ function change-name {
     Remove-Item 'C:\stepfile\2.txt'
 }
 
-function join-domain {
-    $dc = "testdomain.local"
-    $password = "LazyAdminPwd123!" | ConvertTo-SecureString -asPlainText –Force
-    $user = "$dc\JamieSA"
-    $creds = New-Object System.Management.Automation.PSCredential($user,$password)
-    Remove-Item 'C:\stepfile\3.txt'
-    Add-Computer -DomainName $dc -Credential $creds -Restart -Force -Verbose
-}
-
 
  if (Test-Path C:\stepfile){
     Set-ExecutionPolicy Bypass
@@ -29,7 +20,7 @@ function join-domain {
          set-dns
      }
      if (Test-Path C:\stepfile\3.txt){
-        join-domain   
+        #join-domain   
      }
      if (Test-Path C:\stepfile\4.txt){
         
